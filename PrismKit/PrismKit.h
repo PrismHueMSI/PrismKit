818ad0883dc8119a31c6f65863771b19434675b6
ErrorErrorError <erikbautista15@gmail.com>
2021-08-06 02:37:06 -0500

    Initial Commit

---
+++ PrismKit/PrismKit.h
@@ -0,0 +1,18 @@
+//
+//  PrismKit.h
+//  PrismKit
+//
+//  Created by Erik Bautista on 8/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrismKit.
+FOUNDATION_EXPORT double PrismKitVersionNumber;
+
+//! Project version string for PrismKit.
+FOUNDATION_EXPORT const unsigned char PrismKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrismKit/PublicHeader.h>
+
+
